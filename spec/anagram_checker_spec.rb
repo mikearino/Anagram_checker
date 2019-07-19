4a15bf4270fa84a72079a02fa326d4eaa263435d
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-19 12:14:24 -0700

    write second test, fail then pass.

--- spec/anagram_checker_spec.rb
+++ spec/anagram_checker_spec.rb
@@ -7,4 +7,7 @@ describe('AnagramChecker') do
     expect(AnagramChecker.new("mat", "tom").word_bot("mat", "tom")).to(eq("This is not an anagram"))
   end
 
+  it('returns is an anagram if first word is equal to second word') do
+    expect(AnagramChecker.new("mat", "mat").word_bot("mat", "mat")).to(eq("This is an anagram"))
+  end
 end

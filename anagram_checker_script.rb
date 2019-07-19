13946fa837f3fdb4ef5675a4b9fd61455e9bc3b2
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-19 12:40:02 -0700

    add logic to boot non words.

--- anagram_checker_script.rb
+++ anagram_checker_script.rb
@@ -3,8 +3,20 @@ require './lib/anagram_checker'
 
 puts "Welcome to Anagran Checker! Please enter in your first word!"
 first_word = gets.chomp.gsub(/\s+/, "")
-puts "Please enter in a second word!"
-second_word = gets.chomp.gsub(/\s+/, "")
+if first_word.scan(/[aeiouy]/).count < 1
+  p "Please enter a valid word"
+  return
+else
+  puts "Please enter in a second word!"
+  second_word = gets.chomp.gsub(/\s+/, "")
+  if second_word.scan(/[aeiouy]/).count < 1
+  p "Please enter a valid word"
+  return
+else
+  end
+end
+
+
 find_out = AnagramChecker.new(first_word, second_word)
 
 p find_out.word_bot(first_word, second_word)

2832d58783f8d85ba08b922a535d110b5fe7ffb5
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-19 08:46:52 -0700

    add custom class and write partial script.

--- anagram_checker_script.rb
+++ anagram_checker_script.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env ruby
 
 require './lib/anagram_checker'
+
+puts "Welcome to Anagran Checker! Please enter in your first word!"
+
+@first_word = gets.chomp
+
+puts "Please enter in a second word!"
+
+@second_word = gets.chomp
--- lib/anagram_checker.rb
+++ lib/anagram_checker.rb
@@ -1,2 +1,10 @@
 class AnagramChecker
+  def initialize ()
+    @first_word = first_word
+    @second_word = second_word
+  end
+
+  def word_bot 
+
+  end
 end

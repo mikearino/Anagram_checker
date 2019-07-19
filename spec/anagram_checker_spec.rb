cbf090bb3934472076f3f0d344e02e2394cec354
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-19 11:44:55 -0700

    work on tests.

--- spec/anagram_checker_spec.rb
+++ spec/anagram_checker_spec.rb
@@ -2,8 +2,8 @@ require 'rspec'
 require 'pry'
 require 'anagram_checker'
 
-# describe('AnagramChecker') do
-#   it('') do
-#     expect().to(eq())
-#   end
-# end
+describe('AnagramChecker') do
+  it('returns false if first word is not equal to second word') do
+    expect(AnagramChecker.new("mat", "tom")).to(eq("This is not an anagram"))
+  end
+end

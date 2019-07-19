bad872a267a48628ce9323090704438fc3bf4841
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-19 10:23:55 -0700

    change to different method.

--- lib/anagram_checker.rb
+++ lib/anagram_checker.rb
@@ -5,10 +5,16 @@ class AnagramChecker
   end
 
   def word_bot(first_word, second_word)
-    if @first_word.casecmp?(@second_word) == true
-      p "This is an anagram"
-    elsif @first_word.casecmp?(@second_word) == false
-      p "This is not an anagram"
-    end
+    if first_word.downcase.split("").sort.join == second_word.downcase.split("").sort.join
+    p "This is an anagram"
+  elsif first_word.downcase.split("").sort.join != second_word.downcase.split("").sort.join
+    p "This is not an anagram"
   end
 end
+end
+
+
+# if @first_word.casecmp?(@second_word) == true
+#   p "This is an anagram"
+# elsif @first_word.casecmp?(@second_word) == false
+#   p "This is not an anagram"

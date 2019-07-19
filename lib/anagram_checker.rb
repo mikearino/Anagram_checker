074236dd78bc3c95bc2065c19c27880ef613d10c
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-19 15:45:12 -0700

    refactor code.

--- lib/anagram_checker.rb
+++ lib/anagram_checker.rb
@@ -1,14 +1,14 @@
 class AnagramChecker
   def initialize (first_word, second_word)
-    @first_word = first_word
-    @second_word = second_word
+    @first_word = first_word.downcase.split("").sort.join
+    @second_word = second_word.downcase.split("").sort.join
   end
   def word_bot(first_word, second_word)
-    if first_word.downcase.split("").sort.join == second_word.downcase.split("").sort.join
+    if @first_word == @second_word
       "This is an anagram"
-    elsif first_word.downcase.split("").sort.join.count(second_word.downcase.split("").sort.join) == 0
+    elsif @first_word.count(@second_word) == 0
       "This is an antigram"
-    elsif first_word.downcase.split("").sort.join != second_word.downcase.split("").sort.join
+    elsif @first_word != @second_word
       "This is not an anagram"
     end
   end

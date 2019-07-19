5a51bbf86f054c2ad7b2523c3380f6e3bf9bcb7d
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-19 09:18:15 -0700

    work on comparison operators.

--- anagram_checker_script.rb
+++ anagram_checker_script.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
-
 require './lib/anagram_checker'
 
 puts "Welcome to Anagran Checker! Please enter in your first word!"
-
 @first_word = gets.chomp
-
 puts "Please enter in a second word!"
-
 @second_word = gets.chomp
+
+find_out = AnagramChecker.new(first_word, second_word).word_bot
--- lib/anagram_checker.rb
+++ lib/anagram_checker.rb
@@ -1,10 +1,14 @@
 class AnagramChecker
-  def initialize ()
+  def initialize (first_word, second_word)
     @first_word = first_word
     @second_word = second_word
   end
 
-  def word_bot 
-
+  def word_bot(first_word, second_word)
+    if @first_word.casecmp(@second_word) == 0
+      puts "This is an anagram"
+    elsif @first_word.casecmp(@second_word) == 1
+      puts "This is not an anagram"
+    end
   end
 end
